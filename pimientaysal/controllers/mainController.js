2a971264522b279318b4e39df0f6a783d4366abc
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-09-12 14:40:08 -0300

    corroborando codigo con el github de ramiro

--- pimientaysal/controllers/mainController.js
+++ pimientaysal/controllers/mainController.js
@@ -1,42 +1,49 @@
-let platos = [ 
-    {
-        id: 1,
-        nombre: "Carpaccio fresco",
-        precio: 65.50,
-        descrip: "Entrada Carpaccio de salmón con cítricos"
-    },
-    {
-        id: 2,
-        nombre: "Risotto de berenjena",
-        precio: 47.00,
-        descrip: "Risotto de berenjena y queso de cabra"
-    },
-    {
-        id: 3,
-        nombre: "Mousse de arroz",
-        precio: 27.50,
-        descrip: "Mousse de arroz con leche y aroma de azahar"
-    },
-    {
-        id: 4,
-        nombre: "Espárragos blancos",
-        precio: 37.50,
-        descrip: "Espárragos blancos con vinagreta de verduras y jamón ibérico"
-    }
+let platos = [
+  {
+      id: 1,
+      nombre: 'Carpaccio fresco',
+      precio: 'U$S 65.50',
+      descripcion: 'Entrada Carpaccio de salmón con cítricos',
+      imagen: '/images/Carpaccio-de-salmon.jpg'
+  },
+
+  {
+      id: 2,
+      nombre: 'Risotto de berenjena',
+      descripcion: 'Risotto de berenjena y queso de cabra',
+      precio: 'U$S 47.00',
+      imagen: '/images/Risotto-berenjena-queso-cabra.jpg'
+  },
+
+  {
+      id: 3,
+      nombre: 'Mousse de arroz',
+      descripcion: 'Mousse de arroz con leche y aroma de azahar',
+      precio: 'U$S 27.50',
+      imagen: '/images/Mousse-de-arroz-con-leche.jpg'
+  },
+
+  {
+      id: 4,
+      nombre: 'Espárragos blancos',
+      descripcion: 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+      precio: 'U$S 37.50',
+      imagen: '/images/esparragos.jpg'
+  }
 ]
 
 
 const mainController = {
 
   home: function (req, res) {
-    res.render("index");
+    res.render("index", {platos : platos});
   },
 
   detalleMenu: function (req, res) {
-    let plato = platos.find(plato => {
+    let platoEncontrado = platos.find(plato => {
         return plato.id == req.params.id
     })
-    res.render("detalleMenu");
+    res.render("detalleMenu", {menu: platoEncontrado});
   },
 
 };
